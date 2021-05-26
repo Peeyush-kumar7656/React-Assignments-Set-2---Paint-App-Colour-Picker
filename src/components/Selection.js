d0d1d83159f3bf9aea394e71b79d70268e6d0e72
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-26 12:29:04 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 const Selection = (props) => {
-  let [temp1, temp2] = useState({ background: "" })
-  const { applyColor } = props;
+  const [temp1,temp2] = useState({ background: "" })
+  const {key,applyColor } = props;
   return (
     <div className="fix-box" style={temp1} onClick={()=>{applyColor(temp2)}}>
     <h2 className="subheading">Selection</h2>
